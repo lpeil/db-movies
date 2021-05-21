b5680332063cdc8df01919919d95195902bac0f7
lpeil <luan@peil.dev>
2021-05-21 11:47:11 -0300

    feat: add language in api

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3/',
+  baseURL: process.env.TMDB_URL,
 });
 
 api.defaults.headers.common.Authorization = `Bearer ${process.env.TMDB_TOKEN}`;
+api.defaults.params = {};
+api.defaults.params.language = 'pt-BR';
 
 export default api;
