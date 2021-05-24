9429440ee6ad099614aa20e2c34ff5adbea758af
lpeil <luan@peil.dev>
2021-05-24 13:16:36 -0300

    feat: add loader on image

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Image = ({ path, alt }) => {
+const Image = ({ path, alt, size }) => {
+  const [loaded, setLoaded] = useState(false);
+
   return (
-    <img src={`${process.env.TMDB_IMAGES}w300${path}`} alt={alt} />
-  )
-}
+    <div className="image-container">
+      <img
+        src={`${process.env.TMDB_IMAGES}${size}${path}`}
+        alt={alt}
+        className={!loaded ? 'hidden' : null}
+        onLoad={() => setLoaded(true)}
+      />
+      {
+        !loaded
+          ? <div className="loading" />
+          : null
+      }
+
+    </div>
+  );
+};
 
 Image.propTypes = {
   path: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
+  size: PropTypes.string,
+};
+
+Image.defaultProps = {
+  size: 'w300',
 };
 
 export default Image;
